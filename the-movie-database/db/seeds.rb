0b53546c0e5dfd568e9dc83253beb70e42046cfc
kevinmainaud <kevin.mainaud@gmail.com>
2018-05-01 13:56:01 +0200

    V01

--- the-movie-database/db/seeds.rb
+++ the-movie-database/db/seeds.rb
@@ -1,3 +1,14 @@
+
+Movie.destroy.all
+
+
+Movie.create(title:"Le Patient Anglais",release_year:1996,director:"Anthony Minghella")
+Movie.create(title:"La vie est belle",release_year:1998,director: "Roberto Benigni")
+Movie.create(title:"Gladiator",release_year:2000,director:"Ridley Scott")
+Movie.create(title:"Pulp Fiction",release_year:1994,director: "Quentin Tarantino")
+Movie.create(title: "Mommy",release_year:2014,director:"Xavier Dolan")
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
